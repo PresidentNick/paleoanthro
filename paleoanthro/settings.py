857de632197ace52ee98f9a263459d675bb5adfc
Denne Reed <denne.reed@gmail.com>
2014-09-11 10:23:00 -0500

    Small update to comments in settings.py

--- paleoanthro/settings.py
+++ paleoanthro/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for mysite project.
+Django settings for the paleoanthro project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
